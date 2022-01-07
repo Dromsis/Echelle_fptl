2aa38d99df97f17c37a16fdd5262024918cee801
Pingouin <72406765+pingouinn@users.noreply.github.com>
2022-01-07 21:35:28 +0100

    Modification de l'utilisation de Natives dépréciées
    Modification du GetDistanceBetweenCoords déprécié depuis la build 4096 et peu optimisé en Vdist
    Modification de la détection de l'arrière du véhicule en remplaçant le calcul par un offset d'entité
    Modification du STR '[E]' en ~INPUT_CONTEXT~ reconnu par le jeu comme une touche à afficher

--- client.lua
+++ client.lua
@@ -29,16 +29,16 @@ Citizen.CreateThread(function()
         local closeveh = GetClosestVehicle(playercoord.x,playercoord.y,playercoord.z,10.0,GetHashKey(veh),70)
         if DoesEntityExist(closeveh) then
             local detect = 4.0
-            local d12coord = GetEntityCoords(closeveh) + GetEntityForwardVector(closeveh) * - detect 
-            if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), d12coord.x , d12coord.y, d12coord.z, true) <= 1.5 then
+            local d12coord = GetOffsetFromEntityInWorldCoords(closeveh, 0.0, -detect, 0.0) 
+            if Vdist(GetEntityCoords(GetPlayerPed(-1)), d12coord.x , d12coord.y, d12coord.z) <= 1.5 then
                 local echelle = GetConvertibleRoofState(closeveh)
                 if echelle == 0 then
-                    ESX.ShowHelpNotification("Appuyez sur [E] pour sortir les échelles", false, false, 1)
+                    ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour sortir les échelles", false, false, 1)
                     if IsControlJustPressed(0,51) then
                         LowerConvertibleRoof(closeveh,false)
                     end
                 elseif echelle == 2 then 
-                    ESX.ShowHelpNotification("Appuyez sur [E] pour ranger les échelles", false, true, 1)
+                    ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour ranger les échelles", false, true, 1)
                     if IsControlJustPressed(0,51)then 
                         RaiseConvertibleRoof(closeveh,false)
                     end
